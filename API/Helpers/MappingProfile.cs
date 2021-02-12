d06a8ddb49f2ee7a7123108062972e481f1fb075
mohamed-mahfouz <mohamedmahfouz1616@gmail.com>
2021-02-12 16:37:10 +0200

    end of section 16

--- API/Helpers/MappingProfile.cs
+++ API/Helpers/MappingProfile.cs
@@ -16,6 +16,9 @@ namespace API.Helpers
             .ForMember(x => x.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
 
             CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+            CreateMap<BasketItemDto, BasketItem>();
+
 
         }
 
